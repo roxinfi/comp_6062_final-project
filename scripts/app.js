72b159b1960d2eb13d2cd54e7d213fa3fc406e5f
roxinfi <v_patel233507@fanshaweonline.ca>
2025-08-07 12:20:15 -0400

    update 07-08-2025

--- scripts/app.js
+++ scripts/app.js
@@ -1,72 +1,58 @@
+const errormessage = 'Error in fetching data from API: ';
+
 const app = Vue.createApp({
     data() {
         return {
+
+            myname: 'Vraj Patel',
+            myid: '12434376',
             // Random User Module
-            user: {
-                firstName: '',
-                lastName: '',
-                age: '',
-                picture: ''
-            },
+            user: {},
 
             // Weather Module
             weather: {
-                city: 'London', // Default values for initial load
+                city: 'Cambridge', // Default values for initial load
                 province: 'Ontario',
                 country: 'Canada'
             },
-            weatherData: {
-                temperature: '',
-                wind: '',
-                description: ''
-            },
+            weatherData: {},
 
             // Dictionary Module
-            dictionary: {
-                word: ''
-            },
-            dictionaryData: {
-                phonetic: '',
-                definition: ''
-            }
+            dictionary: {},
+            meaningofword: '' // Default word for initial load
         }
     },
 
     methods: {
         // Fetch Random User
         getRandomUser() {
-            fetch('https://comp6062.liamstewart.ca/random-user-data')
+            const url1 = `https://comp6062.liamstewart.ca/random-user-data`;
+            fetch(url1)
                 .then(response => {return response.json();})
                 .then(data => {
-                    this.user.firstName = data.user_profile.first_name;
-                    this.user.lastName = data.user_profile.last_name;
-                    this.user.age = data.user_profile.age;
-                    this.user.picture = data.user_profile.avatar_url;
+                    this.user = data;
                 })
-                .catch(error => console.error('Error in fetching user data from API:', error));
+                .catch(error => console.error(errormessage, error));
         },
 
         getWeatherData() {
-            const url = `https://comp6062.liamstewart.ca/weather-data?city=${this.weather.city}&province=${this.weather.province}&country=${this.weather.country}`;
-            fetch(url)
+            const url2 = `https://comp6062.liamstewart.ca/weather-data?city=${this.weather.city}&province=${this.weather.province}&country=${this.weather.country}`;
+            fetch(url2)
                 .then(response => {return response.json();})
                 .then(data => {
-                    this.weatherData.temperature = data.weather_data.temperature;
-                    this.weatherData.wind = data.weather_data.wind_speed;
-                    this.weatherData.description = data.weather_data.weather_description;
+                    this.weatherData = data;
                 })
-                .catch(error => console.error('Error in fetching weather data from API:', error));
+                .catch(error => console.error(errormessage, error));
         },
 
         getDefinition() {
-            const url = `https://comp6062.liamstewart.ca/api/define?word=${this.dictionary.word}`;
-            fetch(url)
+            const url3 = `https://comp6062.liamstewart.ca/api/define?word=${this.meaningofword}`;
+            fetch(url3)
                 .then(response => {return response.json();})
                 .then(data => {
-                    this.dictionaryData.phonetic = data.phonetic;
-                    this.dictionaryData.definition = data.definition;
+                    this.dictionary = data;
                 })
-                .catch(error => console.error('Error in fetching dictionary data from API:', error));
+                .catch(error => console.error(errormessage, error));
         }
     },
 
@@ -74,6 +60,7 @@ const app = Vue.createApp({
         // Auto-load on page load
         this.getRandomUser();
         this.getWeatherData();
+        this.getDefinition();
     }
 });
 
